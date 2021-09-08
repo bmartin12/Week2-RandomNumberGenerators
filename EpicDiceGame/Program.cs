9e3a1a738c535474caac91497c1fb47526d87866
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:30:48 +0300

    add random

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mangus osaleb 2 mängjat
+            //iga mängija viska täringu (1-6)
+            //programm kuvab nende visete tulemuse ja kuvab ka, kes on võitja
+            // - ehk kes viskas suuremat
+            
+            Console.WriteLine($"Throw a dice stupid");
+            Console.ReadLine();
+            Console.WriteLine("Rolling...");
+            System.Threading.Thread.Sleep(1000);
+            Random rnd = new Random();
+            int DiceNumber1 = rnd.Next(1, 6);    
+            if (DiceNumber1 == DiceNumber1)
+            {
+                Console.WriteLine($"stupid threw a {DiceNumber1}");
+            }
+
+            
+            Console.WriteLine("Throw a dice dumbdumb");
+            Console.ReadLine();
+            Console.WriteLine("Rolling...");
+            System.Threading.Thread.Sleep(1000);
+            Random rnd1 = new Random();
+            int DiceNumber2 = rnd1.Next(1, 6);
+            if (DiceNumber2 == DiceNumber2)
+            {
+                Console.WriteLine($"dumbdumb threw a {DiceNumber2}");
+            }
+            
+            if (DiceNumber1 > DiceNumber2)
+            {
+                Console.WriteLine("stupid won, gg. :)");
+            }
+            else if (DiceNumber1 < DiceNumber2)
+            {
+                Console.WriteLine("dumbdumb won, gg. :)");
+            }
+            else if (DiceNumber1 == DiceNumber2)
+            {
+                Console.WriteLine("y'all tied, gg. :)");
+            }
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int MyRandomNumber = rnd.Next(1, 10);
+            
+            if (MyRandomNumber > 5)
+            {
+                Console.WriteLine($"Arvuti genereeritud number on {MyRandomNumber}, See on suurem kui 5");
+            }
+            else if (MyRandomNumber < 5)
+            {
+                Console.WriteLine($"Arvuti genereeritud number on {MyRandomNumber}, see väiksem kui 5");
+            }
+            else if(MyRandomNumber == 5)
+            {
+                Console.WriteLine($"Arvuti genereeritud number ongi {MyRandomNumber}");
+            }
+            //programm genereerib juhuslikku numbrit 1-10
+            //programm kontrollib, kas genereeritud number on suurem, kui 5.
+            //kui number on suurem, kui 5. siis konsool kuvab
+            //"juhuslik number on {MyRandomNumber}, see on suurem, kui 5";
+            //kui number on väiksem, kui 5, konsool kuvab
+            //juhuslik number on {MyRandomNumber}, see on väiksem,
+            //kui 5";
+            //kui number on 5, siis konsool kuvab "juhuslik number on 5"
+
+        }
+    }
+}
